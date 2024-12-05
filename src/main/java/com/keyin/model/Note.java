c922d2e397dd70b3ee3473fa16f9d8ab08f02181
Charity Smith <150025121+csmith709@users.noreply.github.com>
2024-12-05 14:10:24 -0330

    Note edit test

--- src/main/java/com/keyin/model/Note.java
+++ src/main/java/com/keyin/model/Note.java
@@ -1,4 +1,12 @@
 package com.keyin.model;
 
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    // Getters and setters
 }
