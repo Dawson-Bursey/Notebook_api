ec4ec41c3cabb2c15617e6420e291b20cc6cacf9
Dawson Bursey <dawson.bursey@keyin.com>
2024-12-05 15:27:58 -0330

    Note.java Getters and setters

--- src/main/java/com/keyin/model/Note.java
+++ src/main/java/com/keyin/model/Note.java
@@ -14,4 +14,28 @@ public class Note {
     private String content;
 
     // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
