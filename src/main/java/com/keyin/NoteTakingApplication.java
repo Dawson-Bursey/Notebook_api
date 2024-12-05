9f6220d7abdd9fee7658723c5e8be37f121f7674
Dawson Bursey <dawson.bursey@keyin.com>
2024-12-05 15:23:26 -0330

    added files

--- src/main/java/com/keyin/NoteTakingApplication.java
+++ src/main/java/com/keyin/NoteTakingApplication.java
@@ -1,4 +1,12 @@
 package com.keyin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NoteTakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteTakingApplication.class, args);
+    }
 }
--- src/main/java/com/keyin/repository/NoteRepository.java
+++ src/main/java/com/keyin/repository/NoteRepository.java
@@ -1,4 +1,7 @@
 package com.keyin.repository;
 
+import com.keyin.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
