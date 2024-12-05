eaf5806f460b6c157b43ca03cba298470225f49c
Dawson Bursey <dawson.bursey@keyin.com>
2024-12-05 15:50:23 -0330

    added files

--- src/main/java/com/keyin/controller/NoteController.java
+++ src/main/java/com/keyin/controller/NoteController.java
@@ -1,4 +1,30 @@
 package com.keyin.controller;
 
+import com.keyin.model.Note;
+import com.keyin.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
 public class NoteController {
+    @Autowired
+    private NoteService noteService;
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @PostMapping
+    public Note createNote(@RequestBody Note note) {
+        return noteService.createNote(note);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNoteById(@PathVariable Long id) {
+        noteService.deleteNoteById(id);
+    }
 }
